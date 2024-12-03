8136a79b63882d37ed51a7605a6c8bdf6475ce05
AAIDANN <114506075+AAIDANN@users.noreply.github.com>
2024-12-03 16:01:35 +0000

    day1 starting changes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import "./style.css";
 import RealTurtle from "real-turtle";
 import { TextInterface } from "text-interface";
-import { runDemo } from "./demo";
+//import { runDemo } from "./demo";
 
 const canvas = document.querySelector("#real-turtle");
 const textDiv = document.querySelector("#app");
@@ -13,16 +13,171 @@ const turtle = new RealTurtle(canvas, {
   * to "await" at the end of each turtle command,
   * but you *will* have to add a turtle.start()
   * call at the end. */
-  async: true,
+  async: false,
   autoStart: false,
 });
 
-const ti = new TextInterface(textDiv, "My Drawing Program");
+const ti = new TextInterface(textDiv, "ATO Turtle Program!");
+ti.output("Welcome to our Jersey Customizer!");
+ti.output("In this website, you will be able to choose between three New England Patriots and watch us draw it using functions!")
 
 /* Comment out the line below once you're writing
  * your own code -- you can also take a look at the
  * code in demo.js for an example. */
-runDemo(turtle, ti);
+//runDemo(turtle, ti);
+/** Mini-Turtle Project **/
+
+/*
+ * Starting and finishing from an "up" direction,
+ * draw a pair of eyes to the right of the turtle,
+ * where each eye is `size` width and `color`.
+ * The total eye width will be size * 2.5
+ */
+const drawOne = (size, color) => {
+  turtle.beginPath();
+  turtle.forward(300);
+  turtle.left(90);
+  turtle.forward(70);
+  turtle.left(60);
+  turtle.forward(70);
+  turtle.left(120);
+  turtle.forward(25);
+  turtle.right(90);
+  turtle.forward(235);
+  turtle.right(90);
+  turtle.forward(50);
+  turtle.left(90);
+  turtle.forward(40);
+  turtle.left(90);
+  turtle.forward(180);
+  turtle.left(90);
+  turtle.forward(40);
+  turtle.left(90);
+  turtle.forward(50);
+  turtle.penUp();
+  turtle.closePath();
+  turtle.setFillStyle("white");
+  turtle.fill();
+};
+turtle.setSpeed(0.98); // from 0 - 1 (0 slow, 1 fast)
+turtle.setLineWidth(10);
+turtle.setStrokeStyle("red");
+turtle.start();
+drawOne();
+
+const drawTwo = (size, color) => {
+  turtle.beginPath();
+  turtle.penDown();
+  turtle.left(180);
+  turtle.forward(50);
+  turtle.left(90);
+  turtle.forward(50);
+  turtle.right(90);
+  turtle.forward(70);
+  turtle.right(90);
+  turtle.forward(90);
+  turtle.right(90);
+  turtle.forward(200);
+  turtle.right(90);
+  turtle.forward(150);
+  turtle.right(45);
+  turtle.forward(75);
+  turtle.right(45);
+  turtle.forward(50);
+  turtle.left(90);
+  turtle.forward(75);
+  turtle.left(90);
+  turtle.forward(45);
+  turtle.left(90);
+  turtle.forward(40);
+  turtle.right(90);
+  turtle.forward(50);
+  turtle.right(90);
+  turtle.forward(80);
+  turtle.right(90);
+  turtle.forward(150);
+  turtle.right(45);
+  turtle.forward(75);
+  turtle.right(45);
+  turtle.forward(50);
+  turtle.right(45);
+  turtle.forward(100);
+  turtle.right(45);
+  turtle.forward(50);
+  turtle.left(90);
+  turtle.forward(100);
+  turtle.closePath();
+  turtle.setFillStyle("white")
+  turtle.fill();
+};
+
+drawTwo();
+
+const drawFour = (size, color) => {
+  turtle.beginPath();
+  turtle.left(180);
+  turtle.forward(200);
+  turtle.left(90);
+  turtle.forward(169);
+  turtle.right(90);
+  turtle.forward(50);
+  turtle.right(35);
+  turtle.forward(280);
+  turtle.right(55);
+  turtle.forward(70);
+  turtle.right(90);
+  turtle.forward(230);
+  turtle.left(90);
+  turtle.forward(70);
+  turtle.right(90);
+  turtle.forward(50);
+  turtle.right(90);
+  turtle.forward(70);
+  turtle.left(90);
+  turtle.forward(195);
+  turtle.right(90);
+  turtle.forward(55);
+  turtle.penUp();
+  turtle.right(90);
+  turtle.forward(250);
+  turtle.left(90);
+  turtle.forward(30);
+  turtle.right(90);
+  turtle.penDown();
+  turtle.forward(100);
+  turtle.left(150);
+  turtle.forward(120);
+  turtle.left(120);
+  turtle.forward(70);
+  turtle.closePath();
+  turtle.setFillStyle("white");
+  turtle.fill();
+};
+drawFour();
+
+// Test code
+
+/* main method -- put all of your work in here */
+const main = async () => {
+  // Get input...
+
+  let size = await ti.promptSquare("How big a square?");
+
+  // Draw turtle stuff
+  drawSquare(size);
+
+  // We need a separate "start" command
+  // after the turtle.
+  turtle.start();
+
+  main();
+};
+
+// Commenting out main for now --
+// we're just testing our function!
+//main();
+//drawSquare(200);
+//turtle.start();
 
 /* We need a separate "start" command
  * if we are NOT in async mode.
